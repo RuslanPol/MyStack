4c43525a63d1c3d480af73ca09d49b4ffa6056cd
RuslanPol <103576799+RuslanPol@users.noreply.github.com>
2024-01-24 22:34:31 +0700

    Class MyStack

---
+++ TestMyStack/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TestMyStack.Properties;
+
+namespace TestMyStack
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyStack<String> stack = new MyStack<string>(5);
+           
+            stack.Push("!");
+            stack.Push("World");
+            stack.Push(" ");
+            stack.Push("Hello");
+            
+            Console.Write(stack.Pop());
+            Console.Write(stack.Pop());
+            Console.Write(stack.Pop());
+            Console.Write(stack.Pop());
+            
+        }
+    }
+}
---
+++ TestMyStack/Properties/MyStack.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace TestMyStack.Properties
+{
+    public class MyStack<T>
+    {
+        private T[] _array;
+        private int _lastElem;
+
+        public MyStack( int lenght)
+        {
+            this._array = new T[lenght];
+            this._lastElem = -1;
+        }
+
+        public void Push(T element)
+        {
+            if (_lastElem == _array.Length - 1)
+            {
+                throw new InvalidOperationException("Stack is full");
+            }
+
+            _array[++_lastElem] = element;
+        }
+
+        public T Pop()
+        {
+            if (_lastElem == -1)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+
+            T element = _array[_lastElem];
+            _array[_lastElem] = default;
+            _lastElem--;
+            return element;
+        }
+    }
+}
